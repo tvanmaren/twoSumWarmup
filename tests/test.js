9b906d33d4752cab8d528b80f9b0678a39890b32
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-05 10:06:44 -0700

    :bug: fix chai deep-equality issue; complete tests

--- tests/test.js
+++ tests/test.js
@@ -7,8 +7,14 @@ const arrayTarget = require('../arrayTarget');
 
 describe('function', function () {
   describe('arrayTarget', function () {
-    it('should return [-1,-1] when no array numbers add up to the target', function () {
-      expect(arrayTarget([1,2,3],0)).to.equal([-1,-1]);
+    it('should return [-1,-1] when no array numbers sum to the target', function () {
+      expect(arrayTarget([1, 2, 3], 0)).to.deep.equal([-1, -1]);
+      expect(arrayTarget([1,2,3],10)).to.deep.equal([-1,-1]);
     });
+    it('should return an array of the indices of the first two numbers that sum to the target',
+      function () {
+        expect(arrayTarget([1, 2, 3], 3)).to.deep.equal([0,1]);
+        expect(arrayTarget([1,2,3], 5)).to.deep.equal([1,2]);
+      });
   });
 });
